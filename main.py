fa8f7633b3b336f13ed78a95d573152c3abad855
Tanmay-R-Choudhary <tanmay.d.choudhary@gmail.com>
2021-12-20 15:25:56 +0530

    Finished basic simulation

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from motor import Motor
 from stage import Stage, Booster
 from assembly import RocketBody
 from simulation import Simulation
-import numpy as np
-import matplotlib.pyplot as plt
+
+# DEFINING COMPONENTS
 
 constant_thrust_test_motor = Motor()
-constant_thrust_test_motor.define_with_constant_values(100, 10, 0.05, 0.03)
+constant_thrust_test_motor.define_with_constant_values(200, 10, 0.05, 0.03)
 
 constant_thrust_test_motor_two = Motor()
-constant_thrust_test_motor_two.define_with_constant_values(100, 10, 0.05, 0.03)
+constant_thrust_test_motor_two.define_with_constant_values(200, 10, 0.05, 0.03)
 
 test_motor_booster = Booster(
     motor=constant_thrust_test_motor,
@@ -22,12 +22,15 @@ test_motor_booster_two = Booster(
     mass=1.0
 )
 
+# DEFINING BODY
+
 test_stage = Stage(1, test_motor_booster, test_motor_booster_two, 2)
 test_stage_two = Stage(2, test_motor_booster)
 
 test_rocket = RocketBody()
 test_rocket.add_stage(test_stage, test_stage_two)
 
-sim = Simulation(test_rocket, 90)
+# DEFINING SIMULATION
 
-print(sim.stage_mass_array)
+sim = Simulation(test_rocket, 90)
+sim.plot_disp_curve()
--- simulation.py
+++ simulation.py
@@ -2,92 +2,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 from functions import *
 
-'''
-class Simulation:
-    def __init__(self, rocket, launch_angle):
-        self.stage_acceleration_list = []
-        self.stage_thrust_list = []
-        self.stage_mass_list = []
-        self.body = rocket
-        self.g = -9.81
-        self.launch_angle = launch_angle
-
-        for stage in self.body.stages:
-            _temp_core_stage_thrust = np.copy(stage.core_stage.motor.thrust)[0]
-            if stage.side_booster is not None:
-                _temp_side_stage_thrust = stage.num_side_boosters * np.copy(stage.side_booster.motor.thrust)[0]
-                _common_thrust = [sum(common_val) for common_val in zip(_temp_core_stage_thrust, _temp_side_stage_thrust)]
-                self.stage_thrust_list.append(
-                    np.append(_common_thrust, _temp_core_stage_thrust[len(_temp_side_stage_thrust) - 1:]))
-            else:
-                self.stage_thrust_list.append(_temp_core_stage_thrust)
-
-        for stage in self.body.stages:
-            _temp_core_stage_mass = np.copy(stage.core_stage.motor.mass)[0]
-            if stage.side_booster is not None:
-                _temp_side_stage_mass = stage.num_side_boosters * np.copy(stage.side_booster.motor.mass)[0]
-                _common_mass = [sum(common_val) for common_val in zip(_temp_core_stage_mass, _temp_side_stage_mass)]
-                self.stage_mass_list.append(
-                    np.append(_common_mass,
-                              _temp_core_stage_mass[
-                              len(_temp_side_stage_mass) - 1:]) + stage.core_stage.mass + stage.num_side_boosters * stage.side_booster.mass
-                )
-            else:
-                self.stage_mass_list.append(_temp_core_stage_mass)
-
-        total_len, time_array = 0, []
-        for stage in self.stage_thrust_list:
-            total_len += len(stage)
-        for i in range(0, total_len):
-            time_array.append(i/10)
-
-        self.time_array = np.array(time_array)
-
-        for i in range(len(self.stage_mass_list)):
-            self.stage_acceleration_list.append(self.stage_thrust_list[i] / self.stage_mass_list[i])
-
-    def plot_acceleration_curve(self, stage_number=None):
-        if stage_number is None:
-            for i, data in enumerate(self.stage_acceleration_list):
-                plt.figure("Stage {}".format(i + 1))
-                plt.plot(data)
-        else:
-            plt.plot(self.stage_acceleration_list[stage_number-1])
-
-        plt.show()
-
-    def plot_velocity_curve(self, stage_number=None):
-        if stage_number is None:
-            for i, data in enumerate(self.stage_acceleration_list):
-                plt.figure("Stage {}".format(i + 1))
-                plt.plot(integrate_graph([i/10 for i in range(0, len(data))], data))
-
-        plt.show()
-'''
 
 class Simulation:
-    def __init__(self, rocket, launch_angle):
+    def __init__(self, rocket, launch_angle, sim_time_after_burnout=500):
         self.body = rocket
         self.stage_thrust_array = []
         self.stage_mass_array = []
+        self.g = -9.81
 
         for stage in self.body.stages:
-            print(stage.stage_number)
             if stage.side_booster is not None:
                 temp_total_thrust = np.zeros((1, stage.core_stage.motor.thrust.shape[1]))
-                temp_core_thrust, temp_side_thrust = np.copy(stage.core_stage.motor.thrust), np.copy(stage.side_booster.motor.thrust)
-                temp_side_thrust = np.append(temp_side_thrust, np.zeros((1, temp_core_thrust.shape[1] - temp_side_thrust.shape[1])))
+                temp_core_thrust, temp_side_thrust = np.copy(stage.core_stage.motor.thrust), np.copy(
+                    stage.side_booster.motor.thrust)
+                temp_side_thrust = np.append(temp_side_thrust,
+                                             np.zeros((1, temp_core_thrust.shape[1] - temp_side_thrust.shape[1])))
                 temp_side_thrust *= stage.num_side_boosters
                 temp_total_thrust += temp_core_thrust + temp_side_thrust
-                self.stage_thrust_array.append(temp_total_thrust)
+                self.stage_thrust_array.append(temp_total_thrust * np.sin(launch_angle * (np.pi / 180)))
 
                 temp_total_mass = np.zeros((1, len(temp_total_thrust[0])))
-                temp_core_mass, temp_side_mass = np.copy(stage.core_stage.motor.mass), np.copy(stage.side_booster.motor.mass)
-                temp_core_mass = np.append(temp_core_mass, np.zeros((1, temp_total_mass.shape[1] - temp_core_mass.shape[1])))
-                temp_side_mass = np.append(temp_side_mass, np.zeros((1, temp_total_mass.shape[1] - temp_side_mass.shape[1])))
+                temp_core_mass, temp_side_mass = np.copy(stage.core_stage.motor.mass), np.copy(
+                    stage.side_booster.motor.mass)
+                temp_core_mass = np.append(temp_core_mass,
+                                           np.zeros((1, temp_total_mass.shape[1] - temp_core_mass.shape[1])))
+                temp_side_mass = np.append(temp_side_mass,
+                                           np.zeros((1, temp_total_mass.shape[1] - temp_side_mass.shape[1])))
                 temp_side_mass *= stage.num_side_boosters
                 temp_total_mass += temp_core_mass + temp_side_mass + stage.core_stage.mass + stage.side_booster.mass * stage.num_side_boosters
                 self.stage_mass_array.append(temp_total_mass)
             else:
-                self.stage_thrust_array.append(np.copy(stage.core_stage.motor.thrust))
+                self.stage_thrust_array.append(np.copy(stage.core_stage.motor.thrust) * np.sin(launch_angle * (np.pi / 180)))
                 self.stage_mass_array.append(np.copy(stage.core_stage.motor.mass + stage.core_stage.mass))
+
+            time_array_length = 0
+
+            for i in self.stage_thrust_array:
+                time_array_length += len(i[0])
+
+            time = [i / 10 for i in range(0, time_array_length + 1 + sim_time_after_burnout)]
+            self.sim_time = np.array([time])
+
+            thrust = []
+            for thrust_array in self.stage_thrust_array:
+                thrust.extend(thrust_array[0])
+
+            extension = [0 for i in range(0, len(self.sim_time[0]) - len(thrust))]
+            thrust.extend(extension)
+
+            self.flight_thrust_array = np.array([thrust])
+
+            mass = []
+            for mass_array in self.stage_mass_array:
+                mass.extend(mass_array[0])
+
+            extension = [mass[-1] for i in range(0, len(self.sim_time[0]) - len(mass))]
+            mass.extend(extension)
+
+            self.flight_mass_array = np.array([mass])
+
+            acc = []
+            for i, thrust_array in enumerate(self.stage_thrust_array):
+                acc.extend(thrust_array[0] / self.stage_mass_array[i][0])
+
+            extension = [0 for i in range(0, len(self.sim_time[0]) - len(acc))]
+            acc.extend(extension)
+
+            self.flight_acc_array = np.array([acc]) + self.g
+
+            self.flight_velocity_array = np.array([integrate_graph(self.sim_time[0], self.flight_acc_array[0])])
+
+            self.flight_displacement_array = np.array([integrate_graph(self.sim_time[0], self.flight_velocity_array[0])])
+
+            del extension, mass, thrust, acc, time, time_array_length
+
+    def plot_thrust_curve(self):
+        plt.plot(self.sim_time[0], self.flight_thrust_array[0])
+        plt.show()
+
+    def plot_mass_curve(self):
+        plt.plot(self.sim_time[0], self.flight_mass_array[0])
+        plt.show()
+
+    def plot_acc_curve(self):
+        plt.plot(self.sim_time[0], self.flight_acc_array[0])
+        plt.show()
+
+    def plot_vel_curve(self):
+        plt.plot(self.sim_time[0], self.flight_velocity_array[0])
+        plt.show()
+
+    def plot_disp_curve(self):
+        plt.plot(self.sim_time[0], self.flight_displacement_array[0])
+        plt.show()
